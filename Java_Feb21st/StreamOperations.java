55361f20ae8ada10758487af2592061ee0eabc1b
Sailakshmi-K <99002137+Sailakshmi-K@users.noreply.github.com>
2022-02-21 17:26:34 +0530

    Update StreamOperations.java

--- Java_Feb21st/StreamOperations.java
+++ Java_Feb21st/StreamOperations.java
@@ -1,20 +1,27 @@
 package Jdk8;
 
+import java.util.List;
 import java.util.ArrayList;
-import java.util.Optional;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamOperations {
     public static void main(String[] args) {
         ArrayList<Integer> al=new ArrayList<>();
+        List<Integer> s=Arrays.asList(1,2,3,4,5,6);
 
         for(int i=1;i<=10;i++){
             al.add(i);
         }
+        System.out.println("*******MATCHING*********");
+        System.out.println(s.stream().anyMatch(i-> i>0));
+        System.out.println(s.stream().allMatch(i-> i<6));
+        System.out.println(s.stream().noneMatch(i-> i>10));
+
         System.out.println("*****Filtering*****");
-        Stream<Integer> s=al.stream().filter(i -> i % 2 == 0);
-        s.forEach(i -> System.out.print(i+" "));
+        Stream<Integer> in=al.stream().filter(i -> i % 2 == 0);
+        in.forEach(i -> System.out.print(i+" "));
 
         System.out.println("\n********MAPPING***********");
         Stream<Integer> s1=al.stream().map(x -> x+10);
@@ -33,6 +40,10 @@ public class StreamOperations {
 }
 
 /*
+*******MATCHING*********
+true
+false
+true
 *****Filtering*****
 2 4 6 8 10
 ********MAPPING***********
