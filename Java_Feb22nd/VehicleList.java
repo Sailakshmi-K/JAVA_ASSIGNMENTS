3ae80061a3b5e54427a4fdf4b0e4af44f324e35f
Sailakshmi-K <99002137+Sailakshmi-K@users.noreply.github.com>
2022-02-22 17:21:42 +0530

    Add files via upload

---
+++ Java_Feb22nd/VehicleList.java
@@ -0,0 +1,11 @@
+package DifferentVehicles;
+
+public class VehicleList {
+    public static void main(String[] args) {
+        System.out.println("Vehicles : Boat,Car,Bike,Ferry...");
+    }
+}
+
+/*
+Vehicles : Boat,Car,Bike,Ferry...
+ */
