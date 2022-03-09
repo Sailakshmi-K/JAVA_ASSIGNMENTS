a435ee6af4d69d0d2a3bcade1a34961871317d72
Sailakshmi-K <99002137+Sailakshmi-K@users.noreply.github.com>
2022-03-09 16:01:43 +0530

    Add files via upload

---
+++ Servlets/LoginSQLContext/src/main/java/com/loginServlet.java
@@ -0,0 +1,60 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class loginServlet
+ */
+//@WebServlet("/loginServlet")
+public class loginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public loginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+response.setContentType("text/html");
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		if(username.equals(password)) {
+			out.println("<h3><i><b>Welcome</b></i><h3>");
+			response.sendRedirect("welcome.html");
+		}
+		else {
+			out.println("Invalid credentials");
+		}
+	}
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
